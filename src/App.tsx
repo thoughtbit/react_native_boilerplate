99ff2ac896b19af24f33a30c46b83c683132f073
moocss <moocss>
2019-05-20 18:05:28 +0800

    SplashScreen

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@
 
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 class App extends React.Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return (
       <View
